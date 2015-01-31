8ab340eae96e17648bd44807f24e5ac6a9a7d35e
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-01-31 16:41:33 -0600

    Write a shapes module on top of sdl2-sys::rect

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+
 extern crate sdl2;
+extern crate "sdl2-sys" as sdl2_sys;
 
 // Re-export some of the symbols in event.rs
 pub use event::{KeyCode,MouseButton,Event};
+pub use shape::{Rect,Point,Polygon};
 pub use window::Window;
 
 mod event;
+mod shape;
 mod window;
 
---
+++ src/shape.rs
@@ -0,0 +1,15 @@
+
+extern crate sdl2;
+extern crate "sdl2-sys" as sdl2_sys;
+
+use std;
+
+// Might as well reuse the SDL2 structs wherever possible
+pub use sdl2_sys::rect::Rect;
+pub use sdl2_sys::rect::Point;
+
+/// Polygon is mostly being set aside for now. May revisit in the future.
+pub struct Polygon {
+    pub points: std::vec::Vec<Point>,
+}
+
