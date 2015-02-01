fc01a7181bf75cf656d684b6eab5ad8c18c1b8c6
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-01-31 21:47:56 -0600

    Write package document in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//!
+//! simple
+//! ======
+//!
+//! The simplest graphics library, inspired by LOVE2D. See the README for more information.
+//!
+//!
+//! ```
+//! let app = simple::Window::new("hello world", 1920, 1080);
+//!
+//! app.set_color(255, 0, 255, 255);
+//! app.draw_rect(simple::Rect{
+//!     x: 100,
+//!     y: 110,
+//!     w: 120,
+//!     h: 130,
+//! });
+//!
+//! while app.next_frame() {}
+//! ```
+//!
 
 extern crate sdl2;
 extern crate "sdl2-sys" as sdl2_sys;
