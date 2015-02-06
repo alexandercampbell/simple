482481ff59b36b481dc03b0349a7ced0b52cf893
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-02-06 13:06:33 -0600

    Simplify `pub use window` in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,7 @@ extern crate "sdl2-sys" as sdl2_sys;
 // Re-export some of the symbols in event.rs
 pub use event::{KeyCode,MouseButton,Event};
 pub use shape::{Rect,Point,Polygon};
-pub use window::Window;
-pub use window::Image;
+pub use window::{Window,Image};
 
 mod event;
 mod shape;
