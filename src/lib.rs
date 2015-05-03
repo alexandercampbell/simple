dbdc96e1afeaa72163aa1f8f6f4136a14086d4e3
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-05-03 14:45:37 -0500

    Fix incorrect comment on `pub use` in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,15 @@
 //!
 //! The simplest graphics library, inspired by LOVE2D. See the README for more information.
 //!
+//! Homepage: https://github.com/alexandercampbell/simple
+//!
 
 extern crate rand;
 extern crate sdl2;
 extern crate sdl2_image;
 extern crate sdl2_sys;
 
-// Re-export some of the symbols in event.rs
+// Re-export some of the symbols from the other modules.
 pub use event::{Key,MouseButton,Event};
 pub use shape::{Rect,Point,Polygon};
 pub use window::{Window,Image};
