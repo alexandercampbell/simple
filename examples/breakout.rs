76b4b90f4679290b0f04428357d55bb74ac68719
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-05-03 21:06:03 -0500

    Add struct Ball with angle movement calculation

--- examples/breakout.rs
+++ examples/breakout.rs
@@ -2,8 +2,36 @@
  * This example is a complete playable game of breakout.
  */
 
+extern crate rand;
+extern crate num;
+use num::Float;
+
 extern crate simple;
 use simple::*;
+
+struct Ball {
+    x: i32,
+    y: i32,
+    speed: i32,
+    angle: f32,
+}
+
+impl Ball {
+    fn update(&mut self) {
+        self.x += (self.speed as f32 * self.angle.sin()) as i32;
+        self.y += (self.speed as f32 * self.angle.cos()) as i32;
+    }
+
+    fn draw(&self, app: &mut Window) {
+        app.set_color(255, 0, 255, 255);
+        app.fill_rect(Rect{
+            x: self.x - 16,
+            y: self.y - 16,
+            w: 32,
+            h: 32,
+        });
+    }
+}
 
 fn main() {
     let mut app = Window::new("Breakout", 1024, 768);
@@ -13,12 +41,21 @@ fn main() {
         w: 100,
         h: 16,
     };
+    let mut ball = Ball{
+        x: 1024/2,
+        y: 768/2,
+        speed: 5,
+        angle: rand::random(),
+    };
 
     while app.next_frame() {
         app.clear();
 
         // move the paddle to the mouse cursor
         player.x += (app.mouse_position().0 - player.w/2 - player.x) / 3;
+
+        ball.update();
+        ball.draw(&mut app);
 
         app.set_color(255,255,255,255);
         app.fill_rect(player);
