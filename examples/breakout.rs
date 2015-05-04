dd6824dd90ea16b3d2922c58c0d810c883344f95
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-05-03 20:59:05 -0500

    Add player Rect to the breakout example
    The player follows the mouse cursor with a simple converging function.

--- examples/breakout.rs
+++ examples/breakout.rs
@@ -7,6 +7,21 @@ use simple::*;
 
 fn main() {
     let mut app = Window::new("Breakout", 1024, 768);
-    while app.next_frame() {};
+    let mut player = Rect{
+        x: 0,
+        y: 700,
+        w: 100,
+        h: 16,
+    };
+
+    while app.next_frame() {
+        app.clear();
+
+        // move the paddle to the mouse cursor
+        player.x += (app.mouse_position().0 - player.w/2 - player.x) / 3;
+
+        app.set_color(255,255,255,255);
+        app.fill_rect(player);
+    };
 }
 
