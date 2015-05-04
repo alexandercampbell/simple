5fce7ce2531373227c3a89d0f60455037d05271a
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-05-03 21:15:51 -0500

    Add collision detection and bounce logic to Ball

--- examples/breakout.rs
+++ examples/breakout.rs
@@ -10,26 +10,47 @@ extern crate simple;
 use simple::*;
 
 struct Ball {
-    x: i32,
-    y: i32,
+    rect: Rect,
     speed: i32,
     angle: f32,
 }
 
 impl Ball {
+    fn new(xpos: i32, ypos: i32) -> Ball {
+        Ball{
+            rect: Rect{
+                x: xpos/2 - 16,
+                y: ypos/2 - 16,
+                w: 32,
+                h: 32,
+            },
+            speed: 5,
+            angle: rand::random(),
+        }
+    }
+
     fn update(&mut self) {
-        self.x += (self.speed as f32 * self.angle.sin()) as i32;
-        self.y += (self.speed as f32 * self.angle.cos()) as i32;
+        self.rect.x += (self.speed as f32 * self.angle.sin()) as i32;
+        self.rect.y += (self.speed as f32 * self.angle.cos()) as i32;
     }
 
     fn draw(&self, app: &mut Window) {
         app.set_color(255, 0, 255, 255);
-        app.fill_rect(Rect{
-            x: self.x - 16,
-            y: self.y - 16,
-            w: 32,
-            h: 32,
-        });
+        app.fill_rect(self.rect);
+    }
+
+    fn intersects(&self, other: &Rect) -> bool {
+        self.rect.has_intersection(other)
+    }
+
+    fn bounce(&mut self, x_bounce: bool, y_bounce: bool) {
+        if x_bounce {
+            // this is almost definitely the wrong formula. TODO: revisit
+            self.angle = 3.141592 - self.angle;
+        }
+        if y_bounce {
+            self.angle = 3.141592 - self.angle;
+        }
     }
 }
 
@@ -41,12 +62,7 @@ fn main() {
         w: 100,
         h: 16,
     };
-    let mut ball = Ball{
-        x: 1024/2,
-        y: 768/2,
-        speed: 5,
-        angle: rand::random(),
-    };
+    let mut ball = Ball::new(1024/2, 768/2);
 
     while app.next_frame() {
         app.clear();
@@ -56,6 +72,10 @@ fn main() {
 
         ball.update();
         ball.draw(&mut app);
+
+        if ball.intersects(&player) {
+            ball.bounce(false, true);
+        }
 
         app.set_color(255,255,255,255);
         app.fill_rect(player);
