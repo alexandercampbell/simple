2edd8a5c10aee55189cfd516e7bd9e58f7a41b30
Alexander Campbell <alexanderhcampbell@gmail.com>
2015-05-07 22:47:06 -0500

    Refactor and document breakout.rs

--- examples/breakout.rs
+++ examples/breakout.rs
@@ -1,5 +1,8 @@
 /*!
- * This example is a complete playable game of breakout.
+ * This example is a very simple, but playable, game of Breakout. The game is controlled with the
+ * mouse.
+ *
+ * The physics are wonky becuase I'm not good at collision code.
  */
 
 extern crate rand;
@@ -9,8 +12,8 @@ use num::Float;
 extern crate simple;
 use simple::*;
 
-const SCREEN_WIDTH:i32 = 1024;
-const SCREEN_HEIGHT:i32 = 768;
+const SCREEN_WIDTH:i32 = 640;
+const SCREEN_HEIGHT:i32 = 480;
 
 /**
  * Ball is a very simple representation of a rectangle with a speed and angle. Ball contains some
@@ -51,7 +54,7 @@ impl Ball {
      * Clamp the ball onto the screen.
      *
      * This is really annoying but it's required because sometimes the angle fuzzing can cause
-     * the ball to be stuct outside of the window.
+     * the ball to be stuck outside of the window.
      */
     fn clamp_on_screen(&mut self) {
         // X-dimension
@@ -76,6 +79,11 @@ impl Ball {
         app.fill_rect(self.rect);
     }
 
+    /**
+     * Transform the current angle of the ball. If x_bounce is true, the ball bounces as if it has
+     * it a vertical wall (the velocity as measured in the x dimension is inverted). The y_bounce
+     * is identical, but for the y dimension.
+     */
     fn bounce(&mut self, x_bounce: bool, y_bounce: bool) {
         // early out
         if !(x_bounce || y_bounce) { return; }
@@ -91,33 +99,46 @@ impl Ball {
     fn intersects(&self, other: &Rect) -> bool { self.rect.has_intersection(other) }
 }
 
+/**
+ * Blocks are the things along the top of the screen that you try to hit with the ball :)
+ */
 struct Block {
     rect: Rect,
+    color: u8,
 }
 const BLOCK_WIDTH:i32 = 48;
 const BLOCK_HEIGHT:i32 = 32;
 
 impl Block {
+    /**
+     * Each newly created block gets its own randomly generated color.
+     */
     fn new(x: i32, y: i32) -> Block {
-        Block{rect: Rect{
-            x: x,
-            y: y,
-            w: BLOCK_WIDTH,
-            h: BLOCK_HEIGHT,
-        }}
+        Block{
+            rect: Rect{
+                x: x,
+                y: y,
+                w: BLOCK_WIDTH,
+                h: BLOCK_HEIGHT,
+            },
+            color: rand::random(),
+        }
     }
 
     fn draw(&self, app: &mut Window) {
-        app.set_color(255, 0, 255, 255);
+        app.set_color(self.color, 255 - self.color, 255, 255);
         app.fill_rect(self.rect);
     }
 }
 
 fn main() {
-    let mut app = Window::new("Breakout", 1024, 768);
+    let mut app = Window::new("Breakout Demo", SCREEN_WIDTH as u16, SCREEN_HEIGHT as u16);
     let mut ball = Ball::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
-    let mut player = Rect{x: 0, y: 700, w: 100, h: 8};
+    let mut player = Rect{x: 0, y: SCREEN_HEIGHT - 32, w: 100, h: 8};
 
+    /*
+     * Set up the blocks along the top of the screen.
+     */
     let mut blocks = vec![];
     let spacing_x = BLOCK_WIDTH*3/2;
     let spacing_y = BLOCK_HEIGHT*3/2;
@@ -127,6 +148,9 @@ fn main() {
         }
     }
 
+    /*
+     * Main loop
+     */
     while app.next_frame() {
         app.clear();
 
@@ -144,6 +168,7 @@ fn main() {
             ball.rect.y = player.y - ball.rect.h;
         }
 
+        // Keep only those blocks that are not touching the ball.
         blocks = blocks.into_iter().filter(|ref b| {
             b.draw(&mut app);
             !ball.intersects(&(b.rect))
